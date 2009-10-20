11c731045899dcffe0e5b616784073fef34ebcf1
Jules Gosnell <jules_gosnell@yahoo.com>
2009-10-20 07:46:38 +0100

    figure out whether Camel proxies are migratable like ours - they are not :-(

--- src/test/java/org/omo/core/CamelTestCase.java
+++ src/test/java/org/omo/core/CamelTestCase.java
@@ -14,16 +14,34 @@ import org.apache.camel.util.jndi.JndiContext;
 
 public class CamelTestCase extends TestCase {
 	
+	Munger munger;
+	String string;
+	String mungedString;
+	JndiContext jndiContext;
+	CamelContext camelContext;
+	ConnectionFactory connectionFactory;
+
 	protected void setUp() throws Exception {
 		super.setUp();
+		munger = new MungerImpl();
+		string = "hello";
+		mungedString = munger.munge(string);
+		jndiContext = new JndiContext();
+		jndiContext.bind("munger", munger);
+		camelContext = new DefaultCamelContext(jndiContext);
+		connectionFactory = new ActiveMQConnectionFactory("vm://localhost?broker.persistent=false&broker.useShutdownHook=false");
+		camelContext.addComponent("test-jms", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
+		camelContext.start();
 	}
 
 	protected void tearDown() throws Exception {
+		camelContext.stop();
 		super.tearDown();
 	}
 
 	public static interface Munger {
 		public String munge(String string);
+		public String munge(Munger munger, String string);
 	}
 	
 	public static class MungerImpl implements Munger {
@@ -31,33 +49,26 @@ public class CamelTestCase extends TestCase {
 			System.out.println("munging: " + string);
 			return string.toUpperCase();
 		}
+		public String munge(Munger munger, String string) {
+			return munger.munge(string);
+		}
 	}
 	
-	public void testRemoting() throws Exception {
-		MungerImpl munger = new MungerImpl();
-		String string = "hello";
-		String mungedString = munger.munge(string);
-		
-		JndiContext jndiContext = new JndiContext();
-		jndiContext.bind("munger", munger);
-		CamelContext camelContext = new DefaultCamelContext(jndiContext);
-		
-		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost?broker.persistent=false&broker.useShutdownHook=false");
-		camelContext.addComponent("test-jms", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
+	public void testBeanToBeanOverQueue() throws Exception {
 
 		camelContext.addRoutes(new RouteBuilder() {
 		    public void configure() {
 		        from("test-jms:queue:test.queue").to("bean:munger");
 		    }
 		});
-		camelContext.start();
-
 
 		Munger proxy = ProxyHelper.createProxy(camelContext.getEndpoint("test-jms:queue:test.queue"), Munger.class);
 		assertTrue(proxy.munge(string).equals(mungedString));
-		assertTrue(proxy.munge(string).equals(mungedString));
-		
-		camelContext.stop();
-		
+		 
+		// can we migrate a proxy and still use it ?
+		//assertTrue(proxy.munge(proxy, string).equals(mungedString));
 	}
+	
+	// TODO: CAMEL proxies are not relocatable (Serialisable)... - can I replace their impl ?
+	
 }
