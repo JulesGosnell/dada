3d5b0ba6a04f879753601421f9b6d9a55968e4a1
Jules Gosnell <jules_gosnell@yahoo.com>
2009-12-16 21:56:51 +0000

    checkpoint

---
+++ src/test/java/org/dada/core/SparseOpenLazyViewTableTestCase.java
@@ -0,0 +1,13 @@
+package org.dada.core;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+import junit.framework.TestCase;
+
+public class SparseOpenLazyViewTableTestCase extends TestCase {
+
+	public void test() {
+		//new SparseOpenLazyViewTable<Integer, View<Integer, Datum<Integer>>>(new ConcurrentHashMap<Integer, View<Integer, Datum<Integer>>>, new Factory<Integer, View<Datum<Integer>>>() {
+		//});
+	}
+}
