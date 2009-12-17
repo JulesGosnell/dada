cb0426e1a967510cb9add4a26672aa14d9accff5
Jules Gosnell <jules_gosnell@yahoo.com>
2009-12-17 08:10:59 +0000

    more coverage

--- src/test/java/org/dada/core/SparseOpenLazyViewTableTestCase.java
+++ src/test/java/org/dada/core/SparseOpenLazyViewTableTestCase.java
@@ -1,5 +1,6 @@
 package org.dada.core;
 
+import java.util.Collection;
 import java.util.concurrent.ConcurrentHashMap;
 
 import junit.framework.TestCase;
@@ -7,14 +8,27 @@ import junit.framework.TestCase;
 public class SparseOpenLazyViewTableTestCase extends TestCase {
 
 	public void test() {
+		
+		final View<Integer, Datum<Integer>> view = new View<Integer, Datum<Integer>>() {
+			@Override
+			public void update(Collection<Update<Datum<Integer>>> insertions, Collection<Update<Datum<Integer>>> updates, Collection<Update<Datum<Integer>>> deletions) {
+			}
+		};
+		
 		ConcurrentHashMap<Integer, View<Integer, Datum<Integer>>> map = new ConcurrentHashMap<Integer, View<Integer, Datum<Integer>>>();
 		
 		Factory<Integer, View<Integer, Datum<Integer>>> factory = new Factory<Integer, View<Integer, Datum<Integer>>>() {
 			@Override
-			public View<Integer, Datum<Integer>> create(Integer key)throws Exception {
-				throw new UnsupportedOperationException("NYI");
+			public View<Integer, Datum<Integer>> create(Integer key) throws Exception {
+				return view;
 			}
 		};
-		new SparseOpenLazyViewTable<Integer, Datum<Integer>>(map, factory);
+		SparseOpenLazyViewTable<Integer, Datum<Integer>> table = new SparseOpenLazyViewTable<Integer, Datum<Integer>>(map, factory);
+		
+		
+		View<Integer, Datum<Integer>> lazyView = table.get(0);
+		lazyView.update(null, null, null); // force lazy to activate and replace itself in table...
+		
+		assertTrue(table.get(0) == view);
 	}
 }
