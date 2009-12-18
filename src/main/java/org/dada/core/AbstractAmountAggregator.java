b44a27dfa9f29ad8d0269ac958b0bb772379859b
Jules Gosnell <jules_gosnell@yahoo.com>
2009-12-18 15:50:57 +0000

    support for aggregating smaller into larger amounts

---
+++ src/main/java/org/dada/core/AbstractAmountAggregator.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright (c) 2009, Julian Gosnell
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.dada.core;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class AbstractAmountAggregator<KI, VI extends Datum<KI>, KO, VO extends Datum<KO>> extends AbstractModel<KO, VO> implements View<KI, VI> {
+
+	public interface Factory<KO, VO,KI> {
+		VO create(KO outputKey, int version, KI inputKey, BigDecimal amount);
+	}
+	
+	private final Collection<Update<VO>> empty = new ArrayList<Update<VO>>();
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+	private final KI inputKey;
+	private final KO outputKey;
+	private final Factory<KO, VO, KI> factory;
+	
+	private int version;
+	private BigDecimal aggregate = BigDecimal.ZERO;
+	
+	public AbstractAmountAggregator(String name, KI inputKey, KO outputKey, Metadata<KO, VO> metadata, Factory<KO, VO, KI> factory) {
+		super(name, metadata);
+		this.inputKey = inputKey;
+		this.outputKey = outputKey;
+		this.factory = factory;
+	}
+
+	protected abstract BigDecimal getInputAmount(VI value);
+	protected abstract BigDecimal getOutputAmount(VO value);
+	
+	@Override
+	public Collection<VO> getData() {
+		int snapshotVersion;
+		BigDecimal snapshotAmount;
+		synchronized (this) {
+			snapshotVersion = version;
+			snapshotAmount = aggregate;
+		}
+		return Collections.singleton(factory.create(outputKey, snapshotVersion, inputKey, snapshotAmount));
+	}
+
+	@Override
+	public void update(Collection<Update<VI>> insertions, Collection<Update<VI>> updates, Collection<Update<VI>> deletions) {
+		BigDecimal delta = BigDecimal.ZERO;
+		for (Update<VI> insertion : insertions) {
+			VI newValue = insertion.getNewValue();
+			BigDecimal amount = getInputAmount(newValue);
+			if (amount == null)
+				logger.warn("null amount: {}", newValue);
+			else
+				delta = delta.add(amount);
+		}
+		for (Update<VI> update : updates) {
+			delta.subtract(getInputAmount(update.getOldValue()));
+			delta.add(getInputAmount(update.getNewValue()));
+		}
+		for (Update<VI> update : updates) {
+			delta.subtract(getInputAmount(update.getOldValue()));
+		}
+		int oldVersion, newVersion;
+		BigDecimal oldAggregate, newAggregate;
+		synchronized (this) {
+			oldVersion = version;
+			oldAggregate = aggregate;
+			newVersion = ++version;
+			newAggregate = (aggregate = aggregate.add(delta));
+		}
+		VO oldValue = factory.create(outputKey, oldVersion, inputKey, oldAggregate);
+		VO newValue = factory.create(outputKey, newVersion, inputKey, newAggregate);
+		Collection<Update<VO>> updatesOut = Collections.singleton(new Update<VO>(oldValue, newValue));
+		notifyUpdate(empty, updatesOut, empty);
+	}
+
+	@Override
+	public void start() {
+	}
+
+	@Override
+	public void stop() {
+	}
+
+}
---
+++ src/main/java/org/dada/core/AbstractAmountMetadata.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2009, Julian Gosnell
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.dada.core;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class AbstractAmountMetadata<K, V extends Datum<K>> implements Metadata<K, V> {
+
+	private static List<String> makeStringList(String... array) {
+		List<String> strings = new ArrayList<String>(array.length);
+		for (String string : array)
+			strings.add(string);
+		return strings;
+	}
+	
+	private final List<String> attributeNames;
+
+	public AbstractAmountMetadata(String keyName, String amountName) {
+		attributeNames = makeStringList(keyName, "Version", amountName);
+	}
+	
+	@Override
+	public List<String> getAttributeNames() {
+		return attributeNames;
+	}
+
+	@Override
+	public Object getAttributeValue(V value, int index) {
+		switch (index) {
+		case 0:
+			return value.getId();
+		case 1:
+			return value.getVersion();
+		case 2:
+			return getAmount(value);
+		default:
+			return null;
+		}
+	}
+
+	@Override
+	public K getKey(V value) {
+		return value.getId();
+	}
+	
+	protected abstract BigDecimal getAmount(V value);
+}
