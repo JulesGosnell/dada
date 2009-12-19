1407b7961ff518c2dc46a655131076ee369fce8d
Jules Gosnell <jules_gosnell@yahoo.com>
2009-12-19 17:32:56 +0000

    test AsynchronousTransport using JMock

--- src/main/java/org/dada/core/AsynchronousTransport.java
+++ src/main/java/org/dada/core/AsynchronousTransport.java
@@ -28,6 +28,15 @@
  */
 package org.dada.core;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * A Transport that returns a proxy for its target, invocations upon which are queued and executed on another thread.
  * It may be sensible to execute invocations returning values directly on the calling Thread.
@@ -38,10 +47,60 @@ package org.dada.core;
  */
 public class AsynchronousTransport<T> implements Transport<T> {
 
+	private final Logger log = LoggerFactory.getLogger(AsynchronousTransport.class);
+	
+	private final ExecutorService executorService;
+	private final Class<?>[] interfaces;
+	
+	public AsynchronousTransport(Class<?>[] interfaces, ExecutorService executorService) {
+		this.interfaces = interfaces;
+		this.executorService = executorService;
+	}
+
+	private class Invocation implements Runnable {
+
+		private final T target;
+		private final Method method;
+		private final Object[] args;
+		
+		protected Invocation(T target, Method method, Object[] args) {
+			this.target = target;
+			this.method = method;
+			this.args = args;
+		}
+		
+		@Override
+		public void run() {
+			try {
+				method.invoke(target, args);
+			} catch (Exception e) {
+				log.error("problem during async invocation ({})", this, e);
+			}
+		}
+		
+		@Override
+		public String toString() {
+			return "<" + getClass().getSimpleName() + ":" + target + "." + method + ": " + Arrays.toString(args) + ">";
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
 	@Override
-	public T decouple(T target) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("NYI");
+	public T decouple(final T target) {
+		InvocationHandler handler = new InvocationHandler() {
+			@Override
+			public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+				if (method.getReturnType() == Void.TYPE && !method.getDeclaringClass().equals(Object.class)) {
+					// async invocation
+					executorService.execute(new Invocation(target, method, args));
+					return null;
+				} else {
+					// sync invocation
+					return method.invoke(target, args);
+				}
+			}
+		};
+		return (T)Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(), interfaces, handler);
 	}
 
 }
---
+++ src/test/java/org/dada/core/AsynchronousTransportTestCase.java
@@ -0,0 +1,178 @@
+/*
+ * Copyright (c) 2009, Julian Gosnell
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.dada.core;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import org.jmock.Expectations;
+import org.jmock.integration.junit3.MockObjectTestCase;
+
+public class AsynchronousTransportTestCase extends MockObjectTestCase {
+
+	public static interface Test {
+		
+		void asyncVoidTest();
+		void asyncExceptionTest() throws Exception;
+		boolean syncTest();
+		
+	}
+	
+	public void testException() {
+		// How do we define a mock object that calls a method on something passed to it...
+		
+		final ExecutorService executorService = new ExecutorService() {
+			
+			@Override
+			public void execute(Runnable command) {
+				command.run();
+			}
+			
+			@Override
+			public <T> Future<T> submit(Runnable task, T result) {
+				throw new UnsupportedOperationException("NYI");
+			}
+			
+			@Override
+			public Future<?> submit(Runnable task) {
+				throw new UnsupportedOperationException("NYI");
+			}
+			
+			@Override
+			public <T> Future<T> submit(Callable<T> task) {
+				throw new UnsupportedOperationException("NYI");
+			}
+			
+			@Override
+			public List<Runnable> shutdownNow() {
+				throw new UnsupportedOperationException("NYI");
+			}
+			
+			@Override
+			public void shutdown() {
+				throw new UnsupportedOperationException("NYI");
+			}
+			
+			@Override
+			public boolean isTerminated() {
+				throw new UnsupportedOperationException("NYI");
+			}
+			
+			@Override
+			public boolean isShutdown() {
+				throw new UnsupportedOperationException("NYI");
+			}
+			
+			@Override
+			public <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+				throw new UnsupportedOperationException("NYI");
+			}
+			
+			@Override
+			public <T> T invokeAny(Collection<? extends Callable<T>> tasks) throws InterruptedException, ExecutionException {
+				throw new UnsupportedOperationException("NYI");
+			}
+			
+			@Override
+			public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException {
+				throw new UnsupportedOperationException("NYI");
+			}
+			
+			@Override
+			public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks) throws InterruptedException {
+				throw new UnsupportedOperationException("NYI");
+			}
+			
+			@Override
+			public boolean awaitTermination(long timeout, TimeUnit unit) throws InterruptedException {
+				throw new UnsupportedOperationException("NYI");
+			}
+		};
+
+		final Test test = (Test)mock(Test.class);
+
+		Transport<Test> transport = new AsynchronousTransport<Test>(new Class<?>[]{Test.class}, executorService);
+		Test proxy = transport.decouple(test);
+		
+        // call a method on target that throws an exception
+        checking(new Expectations(){{
+        	try {
+        		one(test).asyncExceptionTest();
+        		will(throwException(new UnsupportedOperationException()));
+        	} catch (Exception e) {
+        		// ignore
+        	}
+        }});
+        
+        try {
+        	proxy.asyncExceptionTest();
+        	// since the exception is expected to be thrown on another thread, it is caught, reported and swallowed...
+        	assertTrue(true);
+        } catch (Exception e) {
+        	assertTrue(false);
+        }
+	}
+	
+	public void test() {
+		final ExecutorService executorService = (ExecutorService)mock(ExecutorService.class);
+		final Test test = (Test)mock(Test.class);
+
+		Transport<Test> transport = new AsynchronousTransport<Test>(new Class<?>[]{Test.class}, executorService);
+		Test proxy = transport.decouple(test);
+
+		// sync call should be dispatched directly onto test
+        checking(new Expectations(){{
+            one(test).syncTest();
+        }});
+		
+        proxy.syncTest();
+        
+        // async call should be dispatched onto executor service
+        checking(new Expectations(){{
+            one(executorService).execute(with(any(Runnable.class)));
+        }});
+		
+        proxy.asyncVoidTest();
+
+// TODO - fix this
+//        // calling a method defined on Object on the proxy, should be dispatched directly on target
+//        checking(new Expectations(){{
+//            one(test).toString();
+//        }});
+		
+        String string = proxy.toString();
+        
+	}
+}
