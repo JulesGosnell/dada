ebae2876e250ecbbf7aa7d0d1c714e43c1900898
Jules Gosnell <jules_gosnell@yahoo.com>
2010-01-19 20:09:01 +0000

    improve coverage

--- src/main/java/org/dada/core/Update.java
+++ src/main/java/org/dada/core/Update.java
@@ -47,4 +47,46 @@ public class Update<V> implements Serializable {
 	public V getNewValue() {
 		return newValue;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((newValue == null) ? 0 : newValue.hashCode());
+		result = prime * result
+				+ ((oldValue == null) ? 0 : oldValue.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof Update)) {
+			return false;
+		}
+		Update other = (Update) obj;
+		if (newValue == null) {
+			if (other.newValue != null) {
+				return false;
+			}
+		} else if (!newValue.equals(other.newValue)) {
+			return false;
+		}
+		if (oldValue == null) {
+			if (other.oldValue != null) {
+				return false;
+			}
+		} else if (!oldValue.equals(other.oldValue)) {
+			return false;
+		}
+		return true;
+	}
+	
+	
 }
