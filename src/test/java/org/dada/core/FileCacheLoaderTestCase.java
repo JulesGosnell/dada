136710e751e032854eb03bd41eeb37d137a6b350
Jules Gosnell <jules_gosnell@yahoo.com>
2010-02-17 10:25:03 +0000

    lose unused import

--- src/test/java/org/dada/core/FileCacheLoaderTestCase.java
+++ src/test/java/org/dada/core/FileCacheLoaderTestCase.java
@@ -29,9 +29,7 @@
 package org.dada.core;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.Collection;
 import java.util.Collections;
