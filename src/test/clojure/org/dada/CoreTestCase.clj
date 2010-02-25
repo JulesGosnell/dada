dbcaee5c63865d6d2b777eaa90619f5a00983941
Jules Gosnell <jules_gosnell@yahoo.com>
2010-02-25 16:50:39 +0000

    add first real test

--- src/test/clojure/org/dada/CoreTestCase.clj
+++ src/test/clojure/org/dada/CoreTestCase.clj
@@ -5,12 +5,21 @@
     (:gen-class
      :extends junit.framework.TestCase
      ;;:init init
-     :methods [[testCore [] void]]
+     :methods [[testCore [] void] [test-attribute-array [] void]]
      )
-    ;;(:use [])
+    (:use [org.dada.core])
     (:import [junit.framework TestCase] ;;[]
 	     ))
 
 (defn -testCore [this]
   (TestCase/assertTrue (Boolean/TRUE))
   (println "TESTED!"))
+
+(defn -test-attribute-array [this]
+  (let [a (attribute-array :a Integer :b String)]
+    (TestCase/assertTrue (= (alength a) 2))
+    (TestCase/assertTrue (= (aget a 0 0) "java.lang.Integer"))
+    (TestCase/assertTrue (= (aget a 0 1) "a"))
+    (TestCase/assertTrue (= (aget a 1 0) "java.lang.String"))
+    (TestCase/assertTrue (= (aget a 1 1) "b"))
+    ))
