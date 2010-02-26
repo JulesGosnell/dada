a522ecfbd45051a8c7526f9387ef993eae400829
Jules Gosnell <jules_gosnell@yahoo.com>
2010-02-26 00:58:25 +0000

    not ready for attribute-array test yet

--- src/test/clojure/org/dada/CoreTestCase.clj
+++ src/test/clojure/org/dada/CoreTestCase.clj
@@ -5,7 +5,9 @@
     (:gen-class
      :extends junit.framework.TestCase
      ;;:init init
-     :methods [[testCore [] void] [test-attribute-array [] void]]
+     :methods [[testCore [] void]
+	       ;;[test-attribute-array [] void]
+	       ]
      )
     (:use [org.dada.core])
     (:import [junit.framework TestCase] ;;[]
@@ -15,11 +17,11 @@
   (TestCase/assertTrue (Boolean/TRUE))
   (println "TESTED!"))
 
-(defn -test-attribute-array [this]
-  (let [a (attribute-array :a Integer :b String)]
-    (TestCase/assertTrue (= (alength a) 2))
-    (TestCase/assertTrue (= (aget a 0 0) "java.lang.Integer"))
-    (TestCase/assertTrue (= (aget a 0 1) "a"))
-    (TestCase/assertTrue (= (aget a 1 0) "java.lang.String"))
-    (TestCase/assertTrue (= (aget a 1 1) "b"))
-    ))
+;; (defn -test-attribute-array [this]
+;;   (let [a (attribute-array :a Integer :b String)]
+;;     (TestCase/assertTrue (= (alength a) 2))
+;;     (TestCase/assertTrue (= (aget a 0 0) "java.lang.Integer"))
+;;     (TestCase/assertTrue (= (aget a 0 1) "a"))
+;;     (TestCase/assertTrue (= (aget a 1 0) "java.lang.String"))
+;;     (TestCase/assertTrue (= (aget a 1 1) "b"))
+;;     ))
