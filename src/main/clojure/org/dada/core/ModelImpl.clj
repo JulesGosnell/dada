a3e843bae485cda8d9f7545797e0c59b4efa98d1
Jules Gosnell <jules_gosnell@yahoo.com>
2010-04-29 10:19:23 +0100

    move aux fns within closure of ctor to avoid having to pass final vars around in param lists

--- src/main/clojure/org/dada/core/ModelImpl.clj
+++ src/main/clojure/org/dada/core/ModelImpl.clj
@@ -1,6 +1,4 @@
 (ns org.dada.core.ModelImpl
-    (:use org.dada.core)
-    ;;(:as this)
     (:import
      [java.util Collection LinkedHashMap]
      [org.dada.core AbstractModel Metadata Update]
@@ -14,56 +12,61 @@
      )
     )
 
-(defn process-addition [[extant extinct key-fn version-fn i a d] addition]
-  (let [new (.getNewValue addition)
-	key (key-fn new)
-	;;dummy (println "PROCESS:" key new)
-	current (extant key)]
-    (if (nil? current)
-      (let [removed (extinct key)]
-	(if (nil? removed)
-	  [(conj {key new} extant) extinct key-fn version-fn (cons (Update. nil new) i) a d] ;insertion
-	  ;; TODO - not extant but extinct
-	  )
-	)
-      ;; extant - alteration - TODO - check version
-      [(conj {key new} extant) extinct key-fn version-fn i (cons (Update. current new) a) d]
-      )))
-
-(defn process-deletion [[extant extinct key-fn version-fn i a d] addition]
-  ;; ensure row with key 
-  ;; send insertion/alteration upstreamp
-  [extant extinct key-fn version-fn i a d])
-
-(defn process-update [extant extinct key-fn version-fn insertions alterations deletions]
-  (reduce process-deletion
-	  (reduce process-addition
-		  (reduce process-addition
-			  [extant extinct key-fn version-fn '() '() '()]
-			  insertions)
-		  alterations)
-	  deletions))
-
 (defn -init [#^String model-name #^Metadata tgt-metadata #^IFn key-fn #^IFn version-fn]
-  [;; super ctor args
+
+  [ ;; super ctor args
    [model-name tgt-metadata]
    ;; instance state
    (let [tgt-creator (.getCreator tgt-metadata)
+
+	 process-addition 
+	 (fn [[extant extinct i a d] #^Update addition]
+	     (let [new (.getNewValue addition)
+		   key (key-fn new)
+		   ;;dummy (println "PROCESS:" key new)
+		   current (extant key)]
+	       (if (nil? current)
+		 (let [removed (extinct key)]
+		   (if (nil? removed)
+		     [(conj {key new} extant) extinct (cons (Update. nil new) i) a d] ;insertion
+		     ;; TODO - not extant but extinct
+		     )
+		   )
+		 ;; extant - alteration - TODO - check version
+		 [(conj {key new} extant) extinct i (cons (Update. current new) a) d]
+		 )))
+
+	  process-deletion
+	  (fn [[extant extinct i a d] #^Update addition]
+	      ;; ensure row with key 
+	      ;; send insertion/alteration upstreamp
+	      [extant extinct i a d])
+
+	  process-update
+	  (fn [extant extinct insertions alterations deletions]
+	      (reduce process-deletion
+		      (reduce process-addition
+			      (reduce process-addition
+				      [extant extinct '() '() '()]
+				      insertions)
+			      alterations)
+		      deletions))
+
 	 update-fn (fn [[extant extinct] & updates]
-		       (apply process-update extant extinct key-fn version-fn updates))]
+		       (apply process-update extant extinct updates))]
      [(atom [{}{}]) update-fn])
    ])
 
-(defn -getData [this]
+(defn -getData [#^org.dada.core.ModelImpl this]
   (let [[mutable-state] (.state this)
 	[extant extinct] @mutable-state]
     (or (vals extant) '())))
 
-(defn -update [#^AbstractModel this & inputs]
+(defn -update [#^org.dada.core.ModelImpl this & inputs]
   ;;(debug "MODELIMPL -update:" this inputs)
   (let [[mutable-state update-fn] (.state this)
 	;;dummy (debug "MODELIMPL INPUT:" inputs)
-	[_ _ _ _ output-insertions output-alterations output-deletions]
+	[_ _ output-insertions output-alterations output-deletions]
 	(apply swap! mutable-state update-fn inputs)
 	;;dummy (debug "MODELIMPL OUTPUT:" output-insertions output-alterations output-deletions)
 	]
