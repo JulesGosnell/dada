f82a79d22752b87099923c315dcb12c226a07cd4
Jules Gosnell <jules_gosnell@yahoo.com>
2010-05-27 08:42:27 +0100

    register view and dump data into gui

--- dada-swt/src/main/clojure/org/dada/swt/gui.clj
+++ dada-swt/src/main/clojure/org/dada/swt/gui.clj
@@ -1,5 +1,6 @@
 (ns org.dada.swt.gui
     (:import
+     (java.io Serializable)
      (org.eclipse.swt SWT)
      (org.eclipse.swt.widgets Display Shell Table TableColumn TableItem Listener)
      (org.eclipse.swt.layout GridData GridLayout)
@@ -26,13 +27,14 @@
     (.setText column text)
     column))
 
-(defn begin []
+(defn begin [metadata data]
   (let [#^Display display (Display.)
 	#^Shell shell (Shell. display)
 	flags (reduce bit-or [(SWT/MULTI) (SWT/BORDER) (SWT/FULL_SELECTION)])
 	#^Table table (Table. shell flags)
-	;; plug in metadata here...
-	titles [" ", "C", "!", "Description", "Resource", "In Folder", "Location"]
+	
+	;; add metadata
+	titles (map str (.getAttributeKeys metadata))
 	columns (doall (map #(make-column table %) titles))]
     
     (.setLayout shell (GridLayout.))
@@ -40,16 +42,14 @@
     (.setHeaderVisible table true)
     (.setLayoutData table (GridData. (SWT/FILL) (SWT/FILL) true true))
     
-    (dotimes [i 128]
-	(let [#^TableItem item (TableItem. table (SWT/NONE))]
-	  (.setText item 0 "x")
-	  (.setText item 1 "y")
-	  (.setText item 2 "!")
-	  (.setText item 3 "this stuff behaves the way I expect")
-	  (.setText item 4 "almost everywhere")
-	  (.setText item 5 "some.folder")
-	  (.setText item 6 (str "line " i " in nowhere"))
-	  ))
+    ;; add data
+    (doall		   ;TODO - use a fn that implies a side-effect
+     (map
+      (fn [datum]
+	  (let [#^TableItem item (TableItem. table (SWT/NONE))]
+	    (.setText item 0 (str datum))
+	    ))
+      data))
     
     (doall (map	#(.pack %) columns))
      
@@ -61,7 +61,7 @@
     (.addListener
      table
      (SWT/DefaultSelection)
-     (proxy [Listener] [] (handleEvent [evt] (println "DefaultSelection" (.getSelection table)))))
+     (proxy [Listener] [] (handleEvent [evt] (println "DefaultSelection" (map #(.getText %) (.getSelection table))))))
 
     ;; insert item at index
     ;;	TableItem item = new TableItem (table, SWT.NONE, 1);
@@ -74,11 +74,65 @@
     (.open shell)
     (swt-loop display shell)))
 
-(begin)
-
 ;; Executing code from a non-UI thread
 
 ;; Applications that wish to call UI code from a non-UI thread must provide a Runnable that calls the UI code. The methods syncExec(Runnable) and asyncExec(Runnable) in the Display class are used to execute these runnables in the UI thread during the event loop.
 
 ;;     * syncExec(Runnable) should be used when the application code in the non-UI thread depends on the return value from the UI code or otherwise needs to ensure that the runnable is run to completion before returning to the thread. SWT will block the calling thread until the runnable has been run from the application's UI thread. For example, a background thread that is computing something based on a window's current size would want to synchronously run the code to get the window's size and then continue with its computations.
 ;;     * asyncExec(Runnable) should be used when the application needs to perform some UI operations, but is not dependent upon the operations being completed before continuing. For example, a background thread that updates a progress indicator or redraws a window could request the update asynchronously and continue with its processing. In this case, there is no guaranteed relationship between the timing of the background thread and the execution of the runnable.
+
+
+(import org.apache.activemq.ActiveMQConnectionFactory)
+(import javax.jms.ConnectionFactory)
+(import javax.jms.Connection)
+(import javax.jms.Session)
+(import org.dada.jms.RemotingFactory)
+(import org.dada.core.MetaModel)
+(import org.dada.core.View)
+(import java.rmi.server.UID)
+(import java.util.concurrent.ExecutorService)
+(import java.util.concurrent.Executors)
+
+
+(def server-name "Cetacea.MetaModel")
+(def url "tcp://localhost:61616")
+(def connection-factory (ActiveMQConnectionFactory. url))
+(def connection (.createConnection connection-factory))
+(.start connection)
+(def session (.createSession connection false (Session/AUTO_ACKNOWLEDGE)))
+
+;; create proxy to server-side metamodel
+
+(def clientside-metamodel-proxy
+     (.createSynchronousClient 
+      (RemotingFactory. session MetaModel, 10000) 
+      (.createQueue session server-name)
+      true))
+
+;; create proxy for ourselves to send to server
+
+(def client-destination (.createQueue session (str "Client" (UID.))))
+(def client-view (proxy [View] [] (update [insertions alterations deletions]
+					  (println ("VIEW:" insertions alterations deletions)))))
+
+(def view-remoting-factory (RemotingFactory. session View 10000))
+
+(def clientside-view-server
+     (.createServer
+      view-remoting-factory 
+      client-view
+      client-destination
+      (Executors/newFixedThreadPool 20)))
+
+(def serverside-view-proxy
+     (.createSynchronousClient
+      view-remoting-factory
+      client-destination
+      true))
+
+;; register our interest in the metamodel
+(def registration (.registerView clientside-metamodel-proxy server-name serverside-view-proxy))
+
+(begin (.getMetadata registration) (.getData registration))
+
+
