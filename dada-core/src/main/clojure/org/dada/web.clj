9abba4df28110a2de2abb5c8c5e267dc82498426
Jules Gosnell <jules_gosnell@yahoo.com>
2010-06-09 08:21:37 +0100

    add type hints

--- dada-core/src/main/clojure/org/dada/web.clj
+++ dada-core/src/main/clojure/org/dada/web.clj
@@ -6,33 +6,36 @@
 	   Request Server]
 	  [org.eclipse.jetty.server.handler
 	   AbstractHandler]
+	  [java.io
+	   OutputStream]	  
 	  [javax.servlet.http
 	   HttpServletRequest HttpServletResponse])
  )
 
 
-(def *jetty* nil)
+(def #^Server *jetty* nil)
 
 (defn start-jetty [port]
   (let [jetty (Server. port)
 	handler (proxy [AbstractHandler] []
-		    (handle [#^String target
-			     #^Request base-request
-			     #^HttpServletRequest request
-			     #^HttpServletResponse response] 
-			    (let [path (.getPathInfo base-request)
-				  name (.substring path (+ (.lastIndexOf path "/") 1))
-				  bytes (@*exported-classes* name)
-				  stream (.getOutputStream response)]
-			    (.setContentType response "application/binary")
-			    (.setContentLength response (count bytes))
-			    (.setStatus response 200)
-			      (println "Serving" name "=" bytes)
-			      (.write stream bytes)
-			      (.close stream))
-			    (.setHandled request true)))]
+		       (handle [#^String target
+				#^Request base-request
+				#^Request request
+				#^HttpServletResponse response] 
+			       (let [path (.getPathInfo base-request)
+				     name (.substring path (+ (.lastIndexOf path "/") 1))
+				     #^"[B" bytes (@*exported-classes* name)
+				     #^OutputStream stream (.getOutputStream response)]
+				 (.setContentType response "application/binary")
+				 (.setContentLength response (count bytes))
+				 (.setStatus response 200)
+				 (println "Serving" request)
+				 (println "Serving" name "=" bytes)
+				 (.write stream bytes)
+				 (.close stream))
+			       (.setHandled request true)))]
     (.setHandler jetty handler)
-    (def *jetty* jetty)
+    (def #^Server *jetty* jetty)
     (.start jetty)))
 
 (defn stop-jetty []
