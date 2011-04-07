5dc82d997bca4344e70989d9d635ac3ceb137412
Jules Gosnell <jules_gosnell@yahoo.com>
2011-04-07 07:48:48 +0100

    migrate  demo server code back into core so it can be reused

---
+++ dada-core/src/main/clojure/org/dada/core/server.clj
@@ -0,0 +1,77 @@
+(ns ^{:author "Jules Gosnell" :doc "Client/Server code for DADA interfaces"} org.dada.core.server
+    (:use
+     [org.dada core]
+     [org.dada.core remote]
+     )
+    (:import
+     [org.dada.core
+      Data
+      Model
+      RemoteView
+      RemoteSession
+      Session
+      SessionImpl
+      SessionManager
+      SessionManagerImpl
+      View]
+     [org.dada.core.remote
+      Remoter]
+     )
+    )
+
+(deftype SessionServer [^Session local ^Remoter remoter queue ^Session remote]
+
+  Session
+
+  (^boolean ping [this]
+	    (.ping local))
+  
+  (^long getLastPingTime [this]
+	 (.getLastPingTime local))
+  
+  (^Data registerView [this ^Model model ^View view]
+	 (if (instance? RemoteView view) (.hack ^RemoteView view remoter))
+	 (.registerView local model view))
+
+  (^Data deregisterView [this ^Model model ^View view]
+	 (if (instance? RemoteView view) (.hack ^RemoteView view remoter))
+	 (.deregisterView local model view))
+
+  (^Model find [this ^Model model key]
+	 (.find local model key))
+
+  (^Data getData [this ^Model model]
+	 (.getData local model))
+
+  (^Model query [this ^String query]
+	  (.query local query))
+
+  (^void close [this]
+	 (.close local)))
+
+(defn session-server [^SessionImpl local ^Remoter remoter]
+  (let [queue (.endPoint remoter)
+	remote (RemoteSession. queue)
+	manager (SessionServer. local remoter queue remote)
+	server (.server remoter manager queue)]
+    (.addCloseHook local (fn [_] (.close server) (.delete queue))) ;TODO - should be done by Remoter
+    remote))
+
+(deftype SessionManagerServer [^SessionManager local ^Remoter remoter]
+
+  SessionManager
+
+  (getName [this]
+	   (.getName local))
+
+  (^Session createSession [this]
+	    (session-server (.createSession local) remoter))
+  
+  (^void close [this]
+	 (.close local)))
+
+(defn  session-manager-server [^SessionManagerImpl local ^Remoter remoter]
+  (let [manager (SessionManagerServer. local remoter)
+	server (.server remoter manager (.endPoint remoter (.getName local)))]
+    (.addCloseHook local (fn [_] (.close server)))
+    manager))
--- dada-demo/src/main/clojure/org/dada/demo/server.clj
+++ dada-demo/src/main/clojure/org/dada/demo/server.clj
@@ -1,98 +1,36 @@
-(ns 
-    #^{:author "Jules Gosnell" :doc "Demo Server for DADA"}
-  org.dada.demo.server
-  (:use
-   [org.dada web]
-   [org.dada core]
-   [org.dada.core remote]
-   [org.dada jms])
-  (:import
-   [java.util.concurrent
-    Executors
-    ExecutorService]
-   [javax.jms
-    ConnectionFactory]
-   [org.springframework.context.support
-    ClassPathXmlApplicationContext]
-   [org.springframework.beans.factory
-    BeanFactory]
-   [org.dada.core
-    Data
-    Model
-    RemoteView
-    RemoteSession
-    Session
-    SessionImpl
-    SessionManager
-    SessionManagerImpl
-    View]
-   [org.dada.core.remote
-    MessageStrategy
-    Remoter
-    SerialiseTranslator
-    Translator]
-   [org.dada.jms
-    BytesMessageStrategy
-    JMSRemoter]
-   )
-  )
+(ns ^{:author "Jules Gosnell" :doc "Server for DADA Demo"} org.dada.demo.server
+    (:use
+     [org.dada web]
+     [org.dada core]
+     [org.dada.core remote]
+     [org.dada.core server]
+     [org.dada jms])
+    (:import
+     [java.util.concurrent
+      Executors
+      ExecutorService]
+     [javax.jms
+      ConnectionFactory]
+     [org.springframework.context.support
+      ClassPathXmlApplicationContext]
+     [org.springframework.beans.factory
+      BeanFactory]
+     [org.dada.core
+      SessionManager
+      SessionManagerImpl
+      ]
+     [org.dada.core.remote
+      MessageStrategy
+      Remoter
+      SerialiseTranslator
+      Translator]
+     [org.dada.jms
+      BytesMessageStrategy
+      JMSRemoter]
+     )
+    )
 
-(deftype SessionServer [^Session local ^Remoter remoter queue ^Session remote]
-
-  Session
-
-  (^boolean ping [this]
-	    (.ping local))
-  
-  (^long getLastPingTime [this]
-	 (.getLastPingTime local))
-  
-  (^Data registerView [this ^Model model ^View view]
-	 (if (instance? RemoteView view) (.hack ^RemoteView view remoter))
-	 (.registerView local model view))
-
-  (^Data deregisterView [this ^Model model ^View view]
-	 (if (instance? RemoteView view) (.hack ^RemoteView view remoter))
-	 (.deregisterView local model view))
-
-  (^Model find [this ^Model model key]
-	 (.find local model key))
-
-  (^Data getData [this ^Model model]
-	 (.getData local model))
-
-  (^Model query [this ^String query]
-	  (.query local query))
-
-  (^void close [this]
-	 (.close local)))
-
-(defn session-server [^SessionImpl local ^Remoter remoter]
-  (let [queue (.endPoint remoter)
-	remote (RemoteSession. queue)
-	manager (SessionServer. local remoter queue remote)
-	server (.server remoter manager queue)]
-    (.addCloseHook local (fn [_] (.close server) (.delete queue))) ;TODO - should be done by Remoter
-    remote))
-
-(deftype SessionManagerServer [^SessionManager local ^Remoter remoter]
-
-  SessionManager
-
-  (getName [this]
-	   (.getName local))
-
-  (^Session createSession [this]
-	    (session-server (.createSession local) remoter))
-  
-  (^void close [this]
-	 (.close local)))
-
-(defn  session-manager-server [^SessionManagerImpl local ^Remoter remoter]
-  (let [manager (SessionManagerServer. local remoter)
-	server (.server remoter manager (.endPoint remoter (.getName local)))]
-    (.addCloseHook local (fn [_] (.close server)))
-    manager))
+;; TODO - most of this should proobably live in spring...
 
 (if (not *compile-files*)
   (do
