c3494957bb9099184da9a4de461b96ff92e9f873
jules <jules_gosnell@yahoo.com>
2012-11-28 23:41:15 +0000

    a bit of a rehash

--- dada-core/src/test/clojure/org/dada/core/test_clojure.clj
+++ dada-core/src/test/clojure/org/dada/core/test_clojure.clj
@@ -1,15 +1,18 @@
 (ns org.dada.core.test-clojure
     (:use 
      [clojure test]
+     [clojure.contrib math]
      )
     )
 
 (set! *warn-on-reflection* true)
 
+;;--------------------------------------------------------------------------------
 ;; test the speed of various bits of clojure to help ascertain which way DADA should do things
 
 (defmacro millis [times body]
   `(do
+     (System/gc)
      (print (quote ~body))
      (let [start# (System/currentTimeMillis)]
        (do (dotimes [_# ~times] ~body) nil)
@@ -17,14 +20,21 @@
 	 (println ": " elapsed# " millis")
 	 elapsed#))))
 
-;; todo - time rhs and lhs on different threads ?
 (defmacro faster [times lhs rhs]
-  `(< (millis ~times ~lhs) (millis ~times ~rhs)))
+  `(let [l# (millis ~times ~lhs) r# (millis ~times ~rhs)] (< l# r#)))
 
-(defrecord Value [v])
+(defmacro slower [times lhs rhs]
+  `(let [l# (millis ~times ~lhs) r# (millis ~times ~rhs)] (> l# r#)))
+
+(defmacro similar [times tolerance lhs rhs]
+  `(let [l# (millis ~times ~lhs) r# (millis ~times ~rhs)] (< (abs (- l# r#)) ~tolerance)))
 
 (defn ibm? []
   (.contains (.toLowerCase (System/getProperty "java.vm.vendor")) "ibm"))
+
+;;--------------------------------------------------------------------------------
+
+(defrecord Value [v])
 
 ;; (if (not (ibm?)) ;TODO - these assumptions do not hold true on IBM - maybe we should generate code accordingly ?
 ;;   (do
@@ -46,16 +56,18 @@
 ;;     (let [^{:tag (type (into-array Object []))} a (into-array Object [0])]
 ;;       (is (faster 100000000 (Value. 0) (.newInstance c a))))))
 
-;; I'm not going to test this - just make a note of it...
-(deftest test-record-vs-pojo
-  (let [^Value r (Value. 0)
-	^String p ""]
-    (faster 100000000 (.v r)(.length p))))
+;; is accessing a record attribute using dot notation comparable in speed to dot notation method invocation ?
+(deftest test-record-access-vs-pojo-method
+  (let [^Value r (Value. 0)] (is (similar 100000000 30 (.v r)(.getClass r)))))
 
-;; this may be needed for where we need to access a field via a function rather than a java accessor... - not much in it...
+;; is accessing a record attribute faster by dot notation than by keyward ?
+;; looks like it is currently 5x faster...
+(deftest test-get
+  (let [^Value v (Value. (Object.))] (is (faster 1000000000 (.v v) (:v v)))))
 
-;; (deftest test-get
-;;   (let [^Value v (Value. 0)] (is (faster 1000000000 ((fn [^Value v] (.v v)) v) (:v v)))))
+;; if we wrap the dot notation in a function, is it still faster ?
+(deftest test-getter
+  (let [^Value v (Value. (Object.)) f (fn [^Value v] (.v v))] (is (faster 1000000000 (f v) (:v v)))))
 
 ;; doseq nearly twice as fast
 
@@ -65,70 +77,54 @@
 
 ;; seems to be true - but so close I can't rely on it not to fail build
 
-;;(deftest assoc-vs-conj
-;;  (is (faster 1 (reduce (fn [r i] (assoc r i i)) {} (range 3000000)) (reduce (fn [r i] (conj r [i i])) {} (range 3000000)))))
+;; (deftest assoc-vs-conj
+;;   (is (let [n 1000000 i (range n)]
+;;         (faster
+;;          1
+;;          (reduce (fn [r i] (assoc r i i)) {} i)
+;;          (reduce (fn [r i] (conj r [i i])) {} i)))))
 
-;; surely some mistake (on my part) here - I can look up a field in a record 100o x faster than I can in an array - I expected it to be faster, but...
-;; this is down to reflection - investigate how to use an Object array efficiently
-;; type hint was wrong - but why ?
+;; There does not seem to be much difference between record attribute
+;; access via dot notation and array access via index. Perhaps rows
+;; should be implemented by arrays ?
 (if (not (ibm?))
   (deftest record-vs-array-access
-    (let [^Value r (Value. 0)
-	  ^objects a (into-array Object [0])]
-      (is (faster 1000000000 (.v r) (aget a 0))))))
+    (let [^Value r (Value. 0) ^objects a (into-array Object [0])] (is (faster 1000000000 (.v r) (aget a 0))))))
 
-;; but accessing a record field still appears to be 10 times faster than an array - is the int being boxed on the way out ?
-(if (not (ibm?))
-  (deftest record-vs-int-array-access
-    (let [^Value r (Value. 0)
-	  ^ints a (int-array [0])]
-      (is (faster 1000000000 (.v r) (aget a 0))))))
-
-;; no it's not autoboxing slowing down the int-array test - array access is slower than record access - by a factor of 10
-(if (not (ibm?))
-  (deftest record-vs-object-array-access
-    (let [^Value r (Value. 0)
-	  ^objects a (object-array [0])]
-      (is (faster 1000000000 (.v r) (aget a 0))))))
 
 (defrecord Foo (^int a))
 (defrecord Bar (^Integer a))
 
-;; int is faster (5x) - to be expected
-
-;; (if (not (ibm?))
-;;   (deftest record-int-vs-integer
-;;     (let [f (Foo. 1)
-;; 	  b (Bar. 1)]
-;;       (is (faster 1000000000 (.a b)  (.a f))))))
+;; reading an int out of a record is about 2x as slow as reading an
+;; Integer - I guess that this is due to autoboxing...
+(if (not (ibm?))
+  (deftest record-int-vs-integer
+    (let [f (Foo. 1) b (Bar. 1)] (is (slower 1000000000 (.a f) (.a b))))))
 
 ;; interesting - reading an int out of a record and pssing it into
 ;; another fn (causing it to be auto-boxed) is still 5-10x faster than
 ;; just reading an Integer out of a record and passing it straight
 ;; into the same fn...
 
-(if (not (ibm?))
-  (deftest record-boxed-int-vs-integer
-    (let [f (Foo. 1)
-	  b (Bar. 1)]
-      (is (faster 1000000000 (identity (.a b))  (identity (.a f)))))))
-  
 (import 'java.util.concurrent.locks.ReadWriteLock)
 (import 'java.util.concurrent.locks.ReentrantReadWriteLock)
 (import 'java.util.concurrent.locks.Lock)
 (import 'java.util.Map)
 (import 'java.util.HashMap)
 
-;; (deftest rw-mutable-vs-immutable
-;;   (let [lock ^ReadWriteLock (ReentrantReadWriteLock.)
-;; 	wr (.writeLock lock)
-;; 	m ^Map (HashMap.)
-;; 	a (atom {})
-;; 	n 10000000]
-;;     (is (faster 1
-;; 		(dotimes [i n] (try (.lock wr) (.put m i i) (catch Exception e)(finally (.unlock wr))))
-;; 		(dotimes [i n] (swap! a conj [i i]))
-;; 		))))
+;; taking a lock, putting an item into a mutable map, releasing the
+;; lock appears quicker than swapping a value onto the end of an
+;; immutable map in an atom...
+(deftest rw-mutable-vs-immutable
+  (let [lock ^ReadWriteLock (ReentrantReadWriteLock.)
+ 	wr (.writeLock lock)
+ 	m ^Map (HashMap.)
+ 	a (atom {})
+ 	n 1000000]
+    (is (faster 1
+ 		(dotimes [i n] (try (.lock wr) (.put m n i) (catch Exception e)(finally (.unlock wr))))
+ 		(dotimes [i n] (swap! a conj [i i]))
+ 		))))
 
 ;; atom 4 times faster on J6/C1.3a4
 ;; (if (not (ibm?))
