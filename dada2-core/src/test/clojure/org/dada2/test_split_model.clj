23e063e439f99f9d9983bc7de8800cf801d0743f
Jules Gosnell <jules_gosnell@yahoo.com>
2013-03-11 08:17:44 +0000

    clean up test before extending

--- dada2-core/src/test/clojure/org/dada2/test_split_model.clj
+++ dada2-core/src/test/clojure/org/dada2/test_split_model.clj
@@ -13,9 +13,8 @@
 (defrecord Employee [name ^int version grade salary])
 
 (deftest test-split-model
-  (let [models (atom nil)
-	new-model-fn (fn [grade change] (first (swap! models conj (versioned-optimistic-map-model :name :version >))))
-	model (split-model :grade new-model-fn)
+  (let [sub-model-fn (fn [grade change] (versioned-optimistic-map-model :name :version >))
+	model (split-model :grade sub-model-fn)
 	view (test-view)
 	james (->Employee :james 0 :developer 50)
 	john  (->Employee :john 0 :developer 50)
@@ -25,9 +24,7 @@
     (is (= nil (data view)))
 
     (on-upsert model james)
-    (is (identical? (first @models) (data view)))
-    (let [{m :developer} (data model)]
-      (is (identical? m (first @models)))
-      ;; now check ms contents...
-      (is (= {:james james} (data m)))
-      )))
+    (let [sub-model (data view)]
+      (is (= {:developer sub-model} (data model)))
+      (is (= {:james james} (data sub-model))))
+    ))
